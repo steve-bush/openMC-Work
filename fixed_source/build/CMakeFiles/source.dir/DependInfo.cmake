a8488f00bf1edae5d6562bd9a49e2d9627ecd099
steve-bush <stevejr@bushinc.com>
2020-09-17 09:04:28 -0500

    Added fixed source examples

---
+++ fixed_source/build/CMakeFiles/source.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/stevejr/Workspace/openMC-Work/fixed_source/source_ring.cpp" "/home/stevejr/Workspace/openMC-Work/fixed_source/build/CMakeFiles/source.dir/source_ring.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GSL_THROW_ON_CONTRACT_VIOLATION"
+  "gsl_CONFIG_ALLOWS_NONSTRICT_SPAN_COMPARISON=1"
+  "gsl_CONFIG_DEFAULTS_VERSION=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/home/stevejr/Opt/anaconda3/envs/openmc/include"
+  "/usr/local/include/faddeeva"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ fixed_source/build/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/source_ring.cpp.o"
+  "libsource.pdb"
+  "libsource.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
